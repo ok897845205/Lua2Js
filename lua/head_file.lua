5bd13589b72204629ce663c2cec24b73a0ded253
Li Zeee <1014891705@qq.com>
2018-11-06 17:29:50 +0800

    add
    add

--- lua/head_file.lua
+++ lua/head_file.lua
@@ -3,13 +3,9 @@ local headFile = {}
 
 -- 游戏 ID 定义
 headFile.GAME_ID = {
-    ZiPai = 1,          -- 字牌
-    NiuNiu = 2,         -- 牛牛
-    PaoDeKuai = 3,      -- 跑得快
-    HongZhong = 4,      -- 红中
-    ChangeSha = 5,      -- 长沙
-    H5HongZhong = 6,     -- H5红中
-    WorldCup = 7        -- 世界杯
+    MaJiang = 1,                  -- 麻将
+    DouDiZhu = 2,                 -- 斗地主
+    NiuNiu = 3,                   -- 牛牛
 }
 
 return headFile
